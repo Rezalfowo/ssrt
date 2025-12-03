96e65aa487572b02f78070917ab3f1aabccfe5cc
Rezalfo <robin.theveniaut@univ-tlse2.fr>
2025-12-03 11:02:34 +0100

    removed generated folders and added script to create the necessary empty directories

---
+++ xtext/createdirs.sh
@@ -0,0 +1,18 @@
+find . -type d -name 'src-gen' -exec rm -r {} +
+find . -type d -name 'xtend-gen' -exec rm -r {} +
+find . -type d -name 'target' -exec rm -r {} +
+
+cd xtext.semcomdt.swarchitecture.cbse.parent
+mkdir xtext.semcomdt.swarchitecture.cbse.ide/xtend-gen
+mkdir xtext.semcomdt.swarchitecture.cbse.ui/xtend-gen
+mkdir xtext.semcomdt.swarchitecture.cbse.web/xtend-gen
+
+cd ../xtext.semcomdt.swsecurity.objective.ciaamps.parent
+mkdir xtext.semcomdt.swsecurity.objective.ciaamps.ide/xtend-gen
+mkdir xtext.semcomdt.swsecurity.objective.ciaamps.ui/xtend-gen
+mkdir xtext.semcomdt.swsecurity.objective.ciaamps.web/xtend-gen
+
+cd ../xtext.semcomdt.swsecurity.ssrt.parent
+mkdir xtext.semcomdt.swsecurity.ssrt.ide/xtend-gen
+mkdir xtext.semcomdt.swsecurity.ssrt.ui/xtend-gen
+mkdir xtext.semcomdt.swsecurity.ssrt.web/xtend-gen
